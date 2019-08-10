427ce54c1ffcf853f9380dfa27b91570e6847742
azenui25 <asanga.nicholas@yahoo.com>
2019-08-10 19:18:02 +0200

    set up js file and cssfile

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,24 @@
 // Write your JS here
+//Hero object
+const hero = {
+    name: "Asanga",
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: "gun",
+        damage: 2
+    }
+}
+
+//Game logic functions
+function rest(hero) {
+    hero.health = 10
+
+}
+
+function pickUpItems(hero) {}
+
+function equipWeapon(hero) {
+
+}
